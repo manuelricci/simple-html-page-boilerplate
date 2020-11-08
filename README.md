d44139c1e7c44ccdc2abf733e88d935669a59814
Manuel Ricci <info@manuelricci.com>
2020-11-08 22:37:00 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Simple HTML Page Boilerplate
+
+Basic config for a simple website.
+
+Install all packages:
+
+```
+npm install
+```
+
+Run webpack
+
+```
+// For production use this command.
+npm run build
+
+// or this one for development.
+npm run serve
+```
+
+Done! You can start your development journey.
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+module.exports = {
+  entry: './src/javascript/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|s)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass')
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css'
+    }),
+    new ESLintPlugin()
+  ]
+}
